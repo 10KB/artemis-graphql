f8e18a671a4a39d0ca66760809644a0d3d4e6e69
Roland Boon <roland@rolandboon.com>
2019-01-29 14:03:09 +0100

    destructure env from context

--- src/plugin.js
+++ src/plugin.js
@@ -149,10 +149,9 @@ function createClient({ link, store, queries, mutations, env }) {
 
 export { createClient };
 
-export default ({ store, nuxtState, isServer }, inject) => {
+export default ({ store, env }, inject) => {
   const queries = require('<%= options.graphqlFolder %>/queries').default;
   const mutations = require('<%= options.graphqlFolder %>/mutations').default;
-  const env = isServer ? process.env : nuxtState.env || {};
 
   const {
     query, q, mutate, m,
